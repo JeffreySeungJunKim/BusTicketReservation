49b83b7972e01477da1a31dd242540362063504c
Jeffrey(Seung Jun) Kim <you7046@gmail.com>
2017-02-12 17:41:27 -0500

    a

---
+++ BusTicketReservation/BusTicketReservation/LoginControl.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusTicketReservation
+{
+    class LoginControl
+    {
+        private static Login _view;//set link to view
+        public void AccountsGroup_Load(object sender,EventArgs e)
+        {
+            _view = (Login)sender;
+        }
+    }
+}
